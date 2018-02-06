e2007082e0df6532f07f509df9bd56231ed77a17
mjw <905396838@qq.com>
2018-02-06 18:02:23 +0800

    20180206

--- src/com/mjw/sinoSpider/test/TestJsoup.java
+++ src/com/mjw/sinoSpider/test/TestJsoup.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,6 +12,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class TestJsoup {
+	
+	private static Logger log = Logger.getLogger(TestJsoup.class);
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -38,8 +41,12 @@ public class TestJsoup {
 					Element a = as.get(0);
 					String url = a.attr("href");
 					String title = a.attr("title");
-					System.out.println("第一次:" + dealByBloomFilter(bf,url));
-					System.out.println("第二次:" + dealByBloomFilter(bf,url));
+					//System.out.println("第一次:" + dealByBloomFilter(bf,url));
+					//System.out.println("第二次:" + dealByBloomFilter(bf,url));
+					if (!dealByBloomFilter(bf, url)) {
+						Long addResult = JedisUtil.setUrlToSet("spiderUrl",url);
+						log.debug(addResult);
+					}
 					News news1 = new News();
 					news1.setUrl(url);
 					news1.setTitle(title);
@@ -54,7 +61,7 @@ public class TestJsoup {
 		boolean flag = false;
 		BloomFilter bF = bf.getBf();
 		flag = bF.contains(url);
-		if(url != null && !url.equals("") && !bF.contains(url))
+		if(url != null && !url.equals("") && !flag)
 			bF.addValue(url);
 		return flag;
 	}
